6ec0eb8a7ec4c2cd8d13d85a9b1112e0996f63ba
Shiv Srivastava <1983.shiv@gmail.com>
2022-01-31 11:46:51 +0530

    revised the footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -6,7 +6,8 @@ const Footer = () => (
   <div className="w-full flex md:justify-center justify-between items-center flex-col p-4 gradient-bg-footer">
     <div className="w-full flex sm:flex-row flex-col justify-between items-center my-4">
       <div className="flex flex-[0.5] justify-center items-center">
-        <img src={logo} alt="logo" className="w-32" />
+        {/* <img src={logo} alt="logo" className="w-32" /> */}
+        <h2 className='text-gray-200 text-bold md:text-4xl text-2xl'>nCRYPTO</h2>
       </div>
       <div className="flex flex-1 justify-evenly items-center flex-wrap sm:mt-0 mt-5 w-full">
         <p className="text-white text-base text-center mx-2 cursor-pointer">BitCoin</p>
